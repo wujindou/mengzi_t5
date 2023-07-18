4a4211fcab535809c9b8ba734c17057c4871c8c2
JackieWu <wujindou1@163.com>
2023-07-18 21:09:00 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,55 @@
+#coding:utf-8
+import sys
+import json
+class Seq2SeqDataset:
+  def __init__(self, data):
+      self.datas = data
+
+  def __len__(self):
+      return len(self.datas)
+
+  def __getitem__(self, index):
+      return self.datas[index]
+
+class DataCollatorForSeq2Seq:
+  def __init__(self, tokenizer, padding: bool = True, max_length: int = 128):
+      self.tokenizer = tokenizer
+      #self.model = model
+      self.max_source_length = 1024
+      self.max_target_length = 512
+      self.padding = padding
+      self.max_length = max_length
+
+  def __call__(self, batch):
+      features = self.collator_fn(batch)
+      return features
+
+
+  def preprocess(self, item):
+      source = item["text"]
+      target = item["entities"]
+      return source, target
+
+  def collator_fn(self, batch):
+      results = map(self.preprocess, batch)
+      inputs, targets = zip(*results)
+
+      input_tensor = self.tokenizer(inputs,
+                                    truncation=True,
+                                    padding=True,
+                                    max_length=self.max_source_length,
+                                    return_tensors="pt",
+                                    )
+
+      target_tensor = self.tokenizer(targets,
+                                      truncation=True,
+                                      padding=True,
+                                      max_length=self.max_target_length,
+                                      return_tensors="pt",
+                                      )
+
+      input_tensor["labels"] = target_tensor["input_ids"]
+
+      if "token_type_ids" in input_tensor:
+          del input_tensor["token_type_ids"]
+      return input_tensor
---
+++ mengzi_t5.py
@@ -0,0 +1,63 @@
+import numpy as np
+import random
+import torch
+from transformers import set_seed
+from dataset import Seq2SeqDataset,DataCollatorForSeq2Seq
+import json
+set_seed(42)
+seed = 42
+random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+np.random.seed(seed)
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.deterministic = True
+
+train_data = json.load(open('./train_webcpm.json','r',encoding='utf-8'))
+dev_data = json.load(open('./dev_webcpm.json','r',encoding='utf-8'))
+from transformers import T5Tokenizer, T5ForConditionalGeneration, TrainingArguments, Trainer
+model_path = "Langboat/mengzi-t5-base" # huggingface下载模型
+Mengzi_tokenizer = T5Tokenizer.from_pretrained(model_path)
+Mengzi_model = T5ForConditionalGeneration.from_pretrained(model_path)
+trainset = Seq2SeqDataset(train_data)
+devset = Seq2SeqDataset(dev_data)
+collator = DataCollatorForSeq2Seq(Mengzi_tokenizer)
+import os
+os.environ["WANDB_DISABLED"] = "true"
+from transformers import T5Tokenizer, T5ForConditionalGeneration, TrainingArguments, Trainer
+output_dir = "test" # 模型checkpoint的保存目录
+training_args = TrainingArguments(
+        num_train_epochs=3,
+        per_device_train_batch_size=1, # batch_size需要根据自己GPU的显存进行设置，2080,8G显存，batch_size设置为2可以跑起来。
+        per_device_eval_batch_size=1,
+        fp16=True,
+        evaluation_strategy="epoch",
+        # eval_steps=100,
+        load_best_model_at_end=True,
+        learning_rate=5e-5,
+        #warmup_steps=100,
+        output_dir="test",
+        save_total_limit=5,
+        lr_scheduler_type='constant',
+        save_strategy='epoch',
+        gradient_accumulation_steps=8,
+        dataloader_num_workers=0,
+        report_to = 'tensorboard',
+        eval_accumulation_steps=2,
+)
+
+#
+
+trainer = Trainer(
+    tokenizer=Mengzi_tokenizer,
+    model=Mengzi_model,
+    args=training_args,
+    data_collator=collator,
+#     data_collator=collator,
+    train_dataset=trainset,
+    eval_dataset=devset
+)
+
+trainer.train()
+trainer.save_model("test/best") # 保存最好的模型
