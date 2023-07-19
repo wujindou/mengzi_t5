af9608281c1ca0ff51f5f744155c41d3537fe7c6
JackieWu <wujindou1@163.com>
2023-07-19 14:26:37 +0800

    Update evaluate_t5.py

--- evaluate_t5.py
+++ evaluate_t5.py
@@ -32,7 +32,7 @@ def inference_with_file(dataset,batch_size=8):
       results.extend(batch_preds)
     return results
 
-dev_data = json.load(open('./dev_webcpm.json','r',encoding='utf-8'))
+dev_data = json.load(open('./dev_webcpm.json','r',encoding='utf-8'))[:5]
 preds = inference_with_file(dev_data)
 print(preds[0])
 targets = [d['answer'] for d in dev_data]
